dabd84d4fcce89f1e390348dd91774bb2c452886
Latn1ck's Archive | БЕЗ БАБ <95050681+Latn1ck@users.noreply.github.com>
2025-02-18 11:06:25 +0300

    Add files via upload

--- черновик.py
+++ черновик.py
@@ -1,22 +1,46 @@
 import pandas as pd
-import xml.etree.ElementTree as ET
+from lxml import etree
 
 
-chunks=pd.read_csv('C:/проект/parsed_tradeitem.csv',sep=';',chunksize=10000)
-
-for chunk in chunks:
-    df=chunk.head(30)
-    break
-print(df['Functionalname'])
-
-""" treeOKRB = ET.parse('OKRB007.xml')
+treeOKRB = etree.parse('OKRB007.xml')
 rootOKRB = treeOKRB.getroot()
-iteratorOKRB=rootOKRB.iter()
-current=next(iteratorOKRB)
-while next(iteratorOKRB) is not None:
+keys=[]
+values=[]
+notFound=0
+for row in rootOKRB.findall('row'):
+    code=row.find('OKRB_CODE')
+    klass=row.find('OKRB_NAME')
+    if code is not None and klass is not None:
+        values.append(code.text)
+        keys.append(klass.text)
+    else:
+        print('Element not found')
+        notFound+=1
+dictOKRB={k:v for k,v in zip(keys,values)}
+print(dictOKRB)
+print(notFound)
+treeGPC = etree.parse('GPC as of November 2021 (GDSN) v20211209 RU.xml')
+rootGPC = treeGPC.getroot()
+iteratorGPC=rootGPC.iter()
+keys=[]
+values=[]
+for i in rootGPC.findall('.//brick'):
+    keys.append(i.attrib['text'])
+    values.append(i.attrib['code'])
+dictBrick={k:v for k,v in zip(keys,values)}
+print(dictBrick)
+keys=[]
+values=[]
+for i in rootGPC.findall('.//class'):
+    keys.append(i.attrib['text'])
+    values.append(i.attrib['code'])
+dictClass={k:v for k,v in zip(keys,values)}
+print(dictClass)
+"""
+while current is not None:
     if current.tag=='OKRB_CODE':
-        res=f'код: {current.text}'
-        current=next(iteratorOKRB)
-        res=f'класс: {current.text}, '+res
-        print(res)
-    current=next(iteratorOKRB) """
+        keys.append(current.text)
+        current=next(iteratorGPC)
+        values.append(current.text)
+    current=next(iteratorGPC)
+ """
