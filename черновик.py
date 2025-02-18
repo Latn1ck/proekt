4efe0295d32d05838815e81270d5f68b39d80a06
Latn1ck's Archive | БЕЗ БАБ <95050681+Latn1ck@users.noreply.github.com>
2025-02-18 12:16:58 +0300

    Add files via upload

--- черновик.py
+++ черновик.py
@@ -1,24 +1,26 @@
 import pandas as pd
 from lxml import etree
-
+import numpy as np
 
 treeOKRB = etree.parse('OKRB007.xml')
 rootOKRB = treeOKRB.getroot()
+print(help(rootOKRB))
 keys=[]
 values=[]
+asd=[]
 notFound=0
 for row in rootOKRB.findall('row'):
     code=row.find('OKRB_CODE')
     klass=row.find('OKRB_NAME')
+    expl=row.find('EXPLANATIONS')
     if code is not None and klass is not None:
-        values.append(code.text)
         keys.append(klass.text)
-    else:
-        print('Element not found')
-        notFound+=1
-dictOKRB={k:v for k,v in zip(keys,values)}
-print(dictOKRB)
-print(notFound)
+        if expl is not None:
+            values.append((code.text,expl.text))
+        else:
+            values.append((code.text,''))
+#dictOKRB={k:v for k,v in zip(keys,values)}
+
 treeGPC = etree.parse('GPC as of November 2021 (GDSN) v20211209 RU.xml')
 rootGPC = treeGPC.getroot()
 iteratorGPC=rootGPC.iter()
@@ -28,14 +30,14 @@ for i in rootGPC.findall('.//brick'):
     keys.append(i.attrib['text'])
     values.append(i.attrib['code'])
 dictBrick={k:v for k,v in zip(keys,values)}
-print(dictBrick)
+#print(dictBrick)
 keys=[]
 values=[]
 for i in rootGPC.findall('.//class'):
     keys.append(i.attrib['text'])
     values.append(i.attrib['code'])
 dictClass={k:v for k,v in zip(keys,values)}
-print(dictClass)
+#print(dictClass)
 """
 while current is not None:
     if current.tag=='OKRB_CODE':
