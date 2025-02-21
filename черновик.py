4c276d6c5b20e4026324f91ea1ff16a95d4343e4
Latn1ck's Archive | БЕЗ БАБ <95050681+Latn1ck@users.noreply.github.com>
2025-02-21 13:49:37 +0300

    Add files via upload

--- черновик.py
+++ черновик.py
@@ -4,23 +4,29 @@ import numpy as np
 
 treeOKRB = etree.parse('OKRB007.xml')
 rootOKRB = treeOKRB.getroot()
-print(help(rootOKRB))
 keys=[]
 values=[]
-asd=[]
 notFound=0
-for row in rootOKRB.findall('row'):
+rows=rootOKRB.findall('row')
+ids=[row.find('OKRB_ID').text for row in rows]
+parents=[row.find('PARENT_ID').text for row in rows]
+parents=[i for i in parents if parents.count(i)==1]
+leaves=[row for row in rows if not (row.find('OKRB_ID').text in parents)]
+for i in leaves:
+    pass
+for row in rows:
     code=row.find('OKRB_CODE')
     klass=row.find('OKRB_NAME')
     expl=row.find('EXPLANATIONS')
+    id=row.find('OKRB_ID')
+    parentID=row.find('PARENT_ID')
     if code is not None and klass is not None:
-        keys.append(klass.text)
+        keys.append(code.text)
         if expl is not None:
-            values.append((code.text,expl.text))
+            values.append((klass.text,expl.text))
         else:
-            values.append((code.text,''))
-#dictOKRB={k:v for k,v in zip(keys,values)}
-
+            values.append((klass.text,''))
+dictOKRB={k:v for k,v in zip(keys,values)}
 treeGPC = etree.parse('GPC as of November 2021 (GDSN) v20211209 RU.xml')
 rootGPC = treeGPC.getroot()
 iteratorGPC=rootGPC.iter()
@@ -30,14 +36,13 @@ for i in rootGPC.findall('.//brick'):
     keys.append(i.attrib['text'])
     values.append(i.attrib['code'])
 dictBrick={k:v for k,v in zip(keys,values)}
-#print(dictBrick)
+
 keys=[]
 values=[]
 for i in rootGPC.findall('.//class'):
     keys.append(i.attrib['text'])
     values.append(i.attrib['code'])
 dictClass={k:v for k,v in zip(keys,values)}
-#print(dictClass)
 """
 while current is not None:
     if current.tag=='OKRB_CODE':
