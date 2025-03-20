bee14f31f1ac28f7ff17d00875c46dd5b953971a
Latn1ck's Archive | БЕЗ БАБ <95050681+Latn1ck@users.noreply.github.com>
2025-03-20 12:44:00 +0300

    Add files via upload

--- chernovik2.py
+++ chernovik2.py
@@ -1,4 +1,3 @@
-import chernovik as ch
 import pandas as pd
 import re
 import numpy as np
@@ -12,21 +11,35 @@ def getChapter(x):
     if x[1] in ['.',',']:
         return '0'+x[0]
     return x[:2]
-
-df=pd.read_csv('parsed_tradeitem.csv',sep=';')
-print(list(set(df['Ticountryoflastprocessing'])))
-nans=pd.DataFrame(df.isna().sum())
+def countryToInt(x):
+    if pd.isna(x):
+        return 0
+    return int(x) if x.isdigit() else int(re.search(r"\[(\d+)\]", x).group(1))
+def f(x):
+    if pd.isna(x):
+        return ''
+    return re.search(r"\[(.*?)\]", x).group(1)
+df=pd.read_csv('parsed_tradeitem.csv',sep=';',quotechar='"', skipinitialspace=True)
+kusok=df.head(100000)
+print(list(set(list(kusok['NetcontentUOM']))))
+kusok['Tradeitemcountryoforigin']=kusok['Tradeitemcountryoforigin'].apply(countryToInt)
+kusok['Tradeitemcountryofassembly']=kusok['Tradeitemcountryofassembly'].apply(countryToInt)
+kusok['Ticountryoflastprocessing']=kusok['Ticountryoflastprocessing'].apply(countryToInt)
+nans=pd.DataFrame(kusok.isna().sum())
 nans=nans.rename(columns={0:'score'})
-nans['Freq']=nans['score']/df.shape[0]
+nans['Freq']=nans['score']/kusok.shape[0]
 nans=nans.T
+nans.to_excel('nans.xlsx')
 threshold=0.9
-vectorDim=10
-train=df.copy()
+train=kusok.copy()
 okrbLabels=train['Okrb007']
 okrbLabels=okrbLabels.apply(getChapter)
-print(okrbLabels)
+train['OkrbLabel']=okrbLabels
 bricks=train['GpcBrick']
 gpcLabels=train[['GpcSegm','GpcFamily','GpcClass']]
 tnvedLabels=train[['Tnvedcode','Tnvedpath']]
-train=train.drop(columns=list(nans.loc[:, nans.loc['Freq']>threshold].columns)+['Okrb007','GpcBrick','GpcSegm','GpcFamily','GpcClass','Tnvedcode','Tnvedpath'])  
+classColumns=['Okrb007Path','Okrb007','GpcBrick','GpcSegm','GpcFamily','GpcClass','Tnvedcode','Tnvedpath']
+unnecColumns=['WeightUOM']
+train=train.drop(columns=list(nans.loc[:, nans.loc['Freq']>threshold].columns)+classColumns+unnecColumns)  
 train=train.sample(n=50000, random_state=42)
+train.to_excel('train.xlsx')
