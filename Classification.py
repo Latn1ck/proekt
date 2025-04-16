8d604205e407b59df2ddd93ec95f0823e488a95b
Latn1ck <95050681+Latn1ck@users.noreply.github.com>
2025-04-16 13:44:10 +0300

    Add files via upload

--- Classification.py
+++ Classification.py
@@ -10,6 +10,7 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from datetime import datetime
+from sklearn.preprocessing import StandardScaler
 
 """ пытаемся классифицировать товары на первом уровне (товару поставить в соответствие раздел). Идея: эмбеддинг+метод классификации из классического машинного обучения """
 def plot_hystogram(a, name): #гистограмма частот для выборки разделов
@@ -29,7 +30,7 @@ def chapterPreprocess(x): #убираем лишнее
 def accuracy(pred,ground_truth): #точность классификации
     return (np.sum(pred==ground_truth))/(len(pred))
 #классы товаров
-dictOKRBexpl=ch.dictOKRBexpl
+""" dictOKRBexpl=ch.dictOKRBexpl
 dictOKRBklass=ch.dictOKRBklass
 shortDictOKRB={}
 for i in dictOKRBexpl.keys():
@@ -37,7 +38,7 @@ for i in dictOKRBexpl.keys():
         shortDictOKRB[i]=chapterPreprocess(dictOKRBexpl[i])
     else:
         shortDictOKRB[i]=chapterPreprocess(dictOKRBklass[i])
-shortDictOKRB={k:chapterPreprocess(v) for k,v in zip(shortDictOKRB.keys(),shortDictOKRB.values()) if len(k)==2}
+shortDictOKRB={k:chapterPreprocess(v) for k,v in zip(shortDictOKRB.keys(),shortDictOKRB.values()) if len(k)==2} """
 #подгрузка датасета
 dataset=ch2.dataset
 X_train, X_test, y_train, y_test = train_test_split(np.array(dataset['sample']), np.array(dataset['label']), test_size=0.2, random_state=42)
@@ -46,34 +47,28 @@ model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniL
 #эмбеддинги описаний классов
 trainEmbeddings=model.encode(X_train)
 testEmbeddings=model.encode(X_test)
-
+#стандартизация
+scaler = StandardScaler()
+train_scaled = scaler.fit_transform(trainEmbeddings)
+test_scaled = scaler.transform(testEmbeddings)
 #логистическая регрессия
 LogRegr=LogisticRegression()
-LogRegr.fit(trainEmbeddings,y_train)
+LogRegr.fit(train_scaled,y_train)
 #предикт
-y_predLogRegr=LogRegr.predict(testEmbeddings)
-#результаты логистической регрессии
-print(f'Logistic Regression Accuracy: {accuracy(y_test,y_predLogRegr)}')
+y_predLogRegr=LogRegr.predict(test_scaled)
 # plot_hystogram(y_test, 'y_test')
 # plot_hystogram(y_predLogRegr, 'Log. Regr.')
 #svm
-svm= SVC(decision_function_shape='ovr',C=1.0,kernel='rbf',gamma='scale',random_state=42)
-svm.fit(trainEmbeddings,y_train)
-y_predSVM=svm.predict(testEmbeddings)
-print(f'SVM Accuracy: {accuracy(y_test,y_predSVM)}')
+param_grid = {'C': [0.1, 1, 10, 100],'kernel': ['rbf', 'poly'],'gamma': ['scale', 'auto', 0.1, 1]}
+svm= SVC(decision_function_shape='ovr',C=10,kernel='rbf',gamma='scale',random_state=42)
+svm.fit(train_scaled,y_train)
+y_predSVM=svm.predict(test_scaled)
 #plot_hystogram(y_predSVM, 'SVM')
-#svm grid
-param_grid = {'C': [0.1, 1, 10, 100],'kernel': ['linear', 'rbf', 'poly'],'gamma': ['scale', 'auto', 0.1, 1]}
-grid_search = GridSearchCV(SVC(), param_grid, cv=5, scoring='accuracy')
-grid_search.fit(trainEmbeddings, y_train)
-best_svm = grid_search.best_estimator_
-print(f"Best parameters: {grid_search.best_params_}")
-print(f"Accuracy: {accuracy(best_svm.predict(testEmbeddings),y_test)}")
+
 #bayes
 gnb = GaussianNB()
-gnb.fit(trainEmbeddings, y_train)
-y_predNB=gnb.predict(testEmbeddings)
-print(f'Bayes Accuracy: {accuracy(y_test,y_predSVM)}')
+gnb.fit(train_scaled, y_train)
+y_predNB=gnb.predict(test_scaled)
 #plot_hystogram(y_predNB, 'Bayes')
 #knn
 best_K=1
@@ -81,14 +76,19 @@ best_acc=0
 print(f'Начало: {datetime.now()}')
 for k in range(1,30):
     knn=KNeighborsClassifier(n_neighbors=k, metric='cosine',weights='distance')
-    knn.fit(trainEmbeddings,y_train)
+    knn.fit(train_scaled,y_train)
     y_predKNN=knn.predict(testEmbeddings)
-    print(f'k={k}, accuracy={accuracy(y_test,y_predKNN)}')
     if accuracy(y_test,y_predKNN)>best_acc:
         best_K=k
         best_acc=accuracy(y_test,y_predKNN)
-print(f'Best KNN Accuracy: {best_acc}, k={best_K}')
+best_KNN=KNeighborsClassifier(n_neighbors=best_K, metric='cosine',weights='distance')
+best_KNN.fit(train_scaled,y_train)
+y_predKNN=best_KNN.predict(test_scaled)
 print(f'Конец: {datetime.now()}')
+accDict={'LogRegr':accuracy(y_test,y_predLogRegr),'SVM':accuracy(y_test,y_predSVM),'bayes':accuracy(y_test,y_predNB),f'KNN, k={best_K}':accuracy(y_test,y_predKNN)}
+print(accDict)
+best = max(accDict, key=accDict.get)
+print(best, f'accuracy={accDict[best]}')
 #plot_hystogram(y_predKNN, 'KNN')
-#res=pd.DataFrame({'sample':X_test, 'true label':y_test,'predictLogRegr':y_predLogRegr, 'predictSVM':y_predSVM, 'predictBayes':y_predNB,'predictKNN':y_predKNN})
-#res.to_excel('res.xlsx')
+res=pd.DataFrame({'sample':X_test, 'true label':y_test,'predictLogRegr':y_predLogRegr, 'predictSVM':y_predSVM, 'predictBayes':y_predNB,'predictKNN':y_predKNN})
+res.to_excel('res.xlsx')
